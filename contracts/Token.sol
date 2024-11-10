a787589b96d8969724a1cb21a8b2e9bdd8ff1b20
Sanjay Goyal <a9266321@gmail.com>
2024-11-10 16:24:51 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("hg","h"){} 
+    constructor()ERC20("hhh","j"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
